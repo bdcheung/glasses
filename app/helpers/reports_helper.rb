2b2b81b95e471bb67b963b92ae299d9b37705b94
Brian Cheung <bdcheung@gmail.com>
2013-05-07 14:12:35 -0400

    Re-added the easter egg

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -3,13 +3,14 @@ module ReportsHelper
 		if @report.daytime?
 			if @report.sunny?
 				"<h1>Yes!</h1>
-				<p>The weather for #{@report.response.location['city']}, #{@report.response.location['region']} is forecast to be #{@report.forecast['text'].downcase}</p>".html_safe
+				<p>The weather for #{@report.response.location['city']}, #{@report.response.location['region']} is forecast to be #{@report.forecast['text'].downcase}.</p>".html_safe
 			else
 				"<h1>No!</h1>
-				<p>The weather for #{@report.response.location['city']}, #{@report.response.location['region']} is forecast to be #{@report.forecast['text'].downcase}</p>".html_safe
+				<p>The weather for #{@report.response.location['city']}, #{@report.response.location['region']} is forecast to be #{@report.forecast['text'].downcase}.</p>".html_safe
 			end
 		else
-			"<h1>No, it's nighttime!</h1>".html_safe
+			"<h1>No, it's nighttime!</h1>
+			<iframe width=420 height=315 src='http://www.youtube.com/embed/XAsq6-jtVD8?autoplay=1' frameborder='0' allowfullscreen></iframe>".html_safe
 		end
 	end
 end
